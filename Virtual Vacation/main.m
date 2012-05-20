6d218c03fe6909fbc0233ca8b2d40b706e5e8ba9
Michael Mangold <michaelmangold@me.com>
2012-05-20 14:17:54 -0400

    Initial Commit

---
+++ Virtual Vacation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Virtual Vacation
+//
+//  Created by Michael Mangold on 5/20/12.
+//  Copyright (c) 2012 Michael Mangold. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
