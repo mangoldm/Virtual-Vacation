e43d976d0df1eb1e7e1e6ea8a4af3ad6e9d58586
Michael Mangold <michaelmangold@me.com>
2012-06-03 23:19:23 -0400

    Re-added deleted vacation files.

--- Virtual Vacation/Vacation+Create.m
+++ Virtual Vacation/Vacation+Create.m
@@ -13,19 +13,19 @@
 
 + (Vacation *)vacationWithName:(NSString *)name inManagedObjectContext:(NSManagedObjectContext *)context
 {
-    Vacation *vacation = nil;
-    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Vacation"];
-    request.predicate = [NSPredicate predicateWithFormat:@"name = %@",name];
+    Vacation *vacation               = nil;
+    NSFetchRequest *request          = [NSFetchRequest fetchRequestWithEntityName:@"Vacation"];
+    request.predicate                = [NSPredicate predicateWithFormat:@"name = %@",name];
     NSSortDescriptor *sortDescriptor = [NSSortDescriptor sortDescriptorWithKey:@"name" ascending:YES];
-    request.sortDescriptors = [NSArray arrayWithObject:sortDescriptor];
+    request.sortDescriptors          = [NSArray arrayWithObject:sortDescriptor];
     
-    NSError *error = nil;
+    NSError *error     = nil;
     NSArray *vacations = [context executeFetchRequest:request error:&error];
     
     if (!vacations || ([vacations count] > 1)) {
-        NSLog(@"Error creating Place.");
+        NSLog(@"Error finding vacation.");
     } else if (![vacations count]) {
-        vacation = [NSEntityDescription insertNewObjectForEntityForName:@"Vacation" inManagedObjectContext:context];
+        vacation      = [NSEntityDescription insertNewObjectForEntityForName:@"Vacation" inManagedObjectContext:context];
         vacation.name = name;
     } else {
         vacation = [vacations lastObject];
@@ -34,28 +34,28 @@
     return vacation;
 }
 
+//// Get the Vacations on file.
+//+ (NSArray *)vacationsOnFile
+//{
+//    // Identify the documents folder URL.
+//    NSFileManager *fileManager = [[NSFileManager alloc] init];
+//    NSError *error             = nil;
+//    NSURL *documentsURL        = [fileManager URLForDirectory:NSDocumentDirectory
+//                                                     inDomain:NSUserDomainMask
+//                                            appropriateForURL:nil
+//                                                       create:NO
+//                                                        error:&error];
+//    if (documentsURL == nil) {
+//        NSLog(@"Could not access documents directory\n%@", [error localizedDescription]);
+//    }
+//    
+//    error              = nil;
+//    NSArray *keys      = [NSArray arrayWithObjects:NSURLNameKey, NSURLTypeIdentifierKey, nil];
+//    NSArray *vacations = [[NSFileManager defaultManager] contentsOfDirectoryAtURL:documentsURL 
+//                                                       includingPropertiesForKeys:keys
+//                                                                          options:0
+//                                                                            error:&error];
+//    return vacations;
+//}
 
-// Get the Vacations on file.
-+ (NSArray *)vacationsOnFile
-{
-    // Identify the documents folder URL.
-    NSFileManager *fileManager = [[NSFileManager alloc] init];
-    NSError *error             = nil;
-    NSURL *documentsURL       = [fileManager URLForDirectory:NSDocumentDirectory
-                                                    inDomain:NSUserDomainMask
-                                           appropriateForURL:nil
-                                                      create:NO
-                                                       error:&error];
-    if (documentsURL == nil) {
-        NSLog(@"Could not access documents directory\n%@", [error localizedDescription]);
-    }
-    
-    error = nil;
-    NSArray *keys = [NSArray arrayWithObjects:NSURLNameKey, NSURLTypeIdentifierKey, nil];
-    NSArray *vacations = [[NSFileManager defaultManager] contentsOfDirectoryAtURL:documentsURL 
-                                                       includingPropertiesForKeys:keys
-                                                                          options:0
-                                                                            error:&error];
-    return vacations;
-}
 @end
