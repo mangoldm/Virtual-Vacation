6644bbd6510af47dabe1ff181acf663a0eea00ce
Michael Mangold <michaelmangold@me.com>
2012-06-20 18:34:32 -0400

    Source control cleanup.

---
+++ Virtual Vacation/TagsViewController.h
@@ -0,0 +1,15 @@
+//
+//  TagsViewController.h
+//  Virtual Vacation
+//
+//  Created by Michael Mangold on 5/22/12.
+//  Copyright (c) 2012 Michael Mangold. All rights reserved.
+//
+//  Displays a table of tags for a single virtual vacation.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TagsViewController : UITableViewController
+
+@end
